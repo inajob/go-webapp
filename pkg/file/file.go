21ab362d0922f8f654a80e4a8f2c13ac3669d9f2
inajob <ina_job@hotmail.com>
2020-03-06 21:57:04 +0900

    fix data path

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -8,11 +8,11 @@ import (
   "mime/multipart"
 )
 
-const BASEDIR = "data" // TODO: only support 1 depth dir
-const IMGDIR = "imgs"
+var CONTENTS_DIR = filepath.Join("data/contents") // TODO: only support 1 depth dir
+var IMG_DIR = filepath.Join("data/imgs")
 
 func SaveImg(user string, id string, imgId string, image multipart.File) (err error){
-  dirPath := filepath.Join(IMGDIR, user, id)
+  dirPath := filepath.Join(IMG_DIR, user, id)
   if _, err := os.Stat(dirPath); err != nil{
     if err := os.MkdirAll(dirPath, 0775); err != nil{
       return err
@@ -32,18 +32,18 @@ func SaveImg(user string, id string, imgId string, image multipart.File) (err er
 }
 
 func GetImgPath(user string, id string, imgId string) (path string){
-  return filepath.Join(IMGDIR, user, id, imgId)
+  return filepath.Join(IMG_DIR, user, id, imgId)
 }
 
 
 func Save (user string, id string, body string) (err error) {
-  dirPath := filepath.Join(BASEDIR, user)
+  dirPath := filepath.Join(CONTENTS_DIR, user)
   if _, err := os.Stat(dirPath); err != nil{
     if err := os.Mkdir(dirPath, 0775); err != nil{
       return err
     }
   }
-  if err := ioutil.WriteFile(filepath.Join(BASEDIR, user, id), []byte(body), 0644); err != nil {
+  if err := ioutil.WriteFile(filepath.Join(CONTENTS_DIR, user, id), []byte(body), 0644); err != nil {
     return err
   }
   return nil
@@ -51,14 +51,14 @@ func Save (user string, id string, body string) (err error) {
 
 func Load (user string, id string) (body []byte, err error) {
   var bytes []byte
-  if bytes, err = ioutil.ReadFile(filepath.Join(BASEDIR, user, id)); err != nil {
+  if bytes, err = ioutil.ReadFile(filepath.Join(CONTENTS_DIR, user, id)); err != nil {
     return nil, err
   }
   return bytes, nil
 }
 
 func List (user string) (files []string, err error) {
-  dirPath := filepath.Join(BASEDIR, user)
+  dirPath := filepath.Join(CONTENTS_DIR, user)
   fs, err := ioutil.ReadDir(dirPath)
   if err != nil {
     return nil, err
