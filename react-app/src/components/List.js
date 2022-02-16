daf1d46270c775a172d10cbdac9b5e835a987ab0
inajob <ina_job@hotmail.com>
2022-02-16 22:39:45 +0900

    hover side link

--- react-app/src/components/List.js
+++ react-app/src/components/List.js
@@ -6,7 +6,7 @@ class List extends React.Component{
       <div className="list">
         <ul>
           {this.props.items.sort((a, b) => b.modTime - a.modTime).slice(0, 20).map((item, index) => (
-            <li key={index}><a href={"?user="+ this.props.user +"&id="+item.name} data-jump={item.name}>{item.name}</a><a href={"?user="+ this.props.user +"&id="+item.name} data-id={item.name}>*</a></li>
+            <li key={index}><a href={"?user="+ this.props.user +"&id="+item.name} data-jump={item.name}>{item.name}</a><a className="non-select" href={"?user="+ this.props.user +"&id="+item.name} data-id={item.name}>*</a></li>
           ))}
         </ul>
       </div>
