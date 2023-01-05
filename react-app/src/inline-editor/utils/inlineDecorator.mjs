e11a425547127455d514f01926d37afa4e838249
inajob <ina_job@hotmail.com>
2023-01-05 17:28:18 +0900

    fix title including space

--- react-app/src/inline-editor/utils/inlineDecorator.mjs
+++ react-app/src/inline-editor/utils/inlineDecorator.mjs
@@ -183,7 +183,7 @@ const API_SERVER=process.env.REACT_APP_API_SERVER
             out.push("<span class='label'>")
             out.push("<span class='invisible'>[</span>")
             out.push("<a class='" + (notFound?"not-found":"") + "' href='?&user=" + user + "&id=" + tmp + "' data-jump='"+ tmp +"'>" + tmp + "</a>");
-            out.push("<span class='invisible'>]</span><a class='non-select side-link' data-id=" + tmp + ">*</a>")
+            out.push("<span class='invisible'>]</span><a class='non-select side-link' data-id=\"" + tmp + "\">*</a>")
             out.push("</span>")
             break;
           default:
