ce48136325d8bf6eea4aee80c104ed6b3bfa6d9c
inajob <ina_job@hotmail.com>
2023-08-25 18:05:28 +0900

    diary page is not important

--- react-app/src/components/InstantSearch.js
+++ react-app/src/components/InstantSearch.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 class InstantSearch extends React.Component{
   render() {
+    let sorter = (list) => {
+      let out = []
+      let out2 = []
+      // push not diary page
+      list.forEach((item) => {
+        if(item.id && null == item.id.match(/\d{4}-\d{2}-\d{2}/)){
+          out.push(item)
+        }else{
+          out2.push(item)
+        }
+      })
+      return out.concat(out2)
+    }
     return (
       <div className="instant-search">
         {Object.keys(this.props.item).map((k, j) => (
           <div className="piece" key={j}>
             <div>Search result of '{k}'</div>
             <div className="pages">
-            {this.props.item[k].slice(0,20).map((r, i) => (
+            {sorter(this.props.item[k]).slice(0,20).map((r, i) => (
               <li key={i}><div><a href={"?user=" + r.user + "&id=" + r.id} data-jump={r.id}>{r.id}</a><a href={"?user=" + r.user + "&id=" + r.id} data-id={r.id}>*</a></div>
                 <div>
                   {(() => {if(r.cover) return <img src={r.cover} alt="cover" />})()}
